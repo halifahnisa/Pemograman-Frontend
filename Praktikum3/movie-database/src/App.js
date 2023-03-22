818b6802f05353d364ff944a19c42be82e467fb2
halifahnisa <92670576+halifahnisa@users.noreply.github.com>
2023-03-22 15:58:09 +0700

    Add files via upload

---
+++ Praktikum3/movie-database/src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Main from './components/Main';
+
+function App() {
+  return (
+    <div>
+      <Hello />
+      <Header />
+      <Footer />
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ Praktikum3/movie-database/src/components/Main.js
@@ -0,0 +1,12 @@
+import Hello from "./Hello";
+
+function Main(){
+    return (
+        <main>
+            <Hello nama="Ridwan" jurusan="Sastra"/>
+            <Hello nama="Peol" jurusan="Informatika"/>
+            <Hello nama="Dani" jurusan="Ilmu Komunikasi"/>
+        </main>
+    );
+};
+export default Main;
