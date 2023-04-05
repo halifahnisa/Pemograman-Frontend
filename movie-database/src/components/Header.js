25aa86af8858f470cd4f54fcf9eb2b6683e5c86d
halifahnisa <92670576+halifahnisa@users.noreply.github.com>
2023-04-05 22:18:18 +0700

    Add files via upload

---
+++ movie-database/src/components/Header.js
@@ -0,0 +1,12 @@
+function Header (){
+    return(
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Abaout</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    );
+};
+export default Header;
