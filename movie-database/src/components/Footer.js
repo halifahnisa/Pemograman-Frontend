7a6c277799a3cc8a37b7bb67209e657c7c8351c9
halifahnisa <92670576+halifahnisa@users.noreply.github.com>
2023-04-05 22:18:45 +0700

    Delete movie-database directory

--- movie-database/src/components/Footer.js
+++
@@ -1,8 +0,0 @@
-function Footer(){
-    return (
-        <footer>
-            <h2>Terimakasi buat kalian yang sudah mencoba belajar React</h2>
-        </footer>
-    );
-};
-export default Footer;
